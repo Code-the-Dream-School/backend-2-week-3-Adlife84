2ff98609b9b33661dc3aa52e4ae9f650e4d2bfe9
Andrei Komolov <andreykomolov@gmail.com>
2020-12-13 21:09:51 -0500

    I've all done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
-    has_many :order
+    has_many :orders, dependent: :destroy
     
     validates :first_name, presence: true
     validates :last_name, presence: true
