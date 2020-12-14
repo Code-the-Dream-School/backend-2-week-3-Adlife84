ded243e28228532b04629e720995b9fd1da155e3
Andrei Komolov <andreykomolov@gmail.com>
2020-12-13 20:02:24 -0500

    Done Rspec Test for Orders Models

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
+  
+  # Create a Order test object for all my test
   subject { Order.new( product_name: "Gears", product_count: 7, customer: FactoryBot.create(:customer))}
+  
   it "is valid with valid attributes" do
     expect(subject).to be_valid
   end
@@ -16,7 +19,15 @@ RSpec.describe Order, type: :model do
     expect(subject).to_not be_valid
   end
 
+  it "is not valid if the product_count is not all digits" do
+    subject.product_count='asd'
+    expect(subject).to_not be_valid
+  end
 
+  it "is not valid without a customer" do
+    subject.customer=nil
+    expect(subject).to_not be_valid
+  end
 
 end
 
